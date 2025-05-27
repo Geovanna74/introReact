d30bd63bf80c93adc4db86a45d2ed1d0142ef5d7
Geovanna74 <geovanacampos1403@gmail.com>
2025-05-26 22:19:14 -0300

    introREACT, Body, Header,Aulas de REACT

---
+++ ola/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+  return (
+    <div className='Body'>
+        <h2 className='titulo2'>Dinos são legais</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, dignissimos obcaecati repellendus adipisci illo quod odio at ipsam quaerat aspernatur ipsum numquam minus aperiam eligendi, dicta eum ullam nesciunt sint.</p>
+    </div>
+  )
+}
+
+export default Body
