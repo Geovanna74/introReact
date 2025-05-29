621d5bc31a06c3eabcc4c1f66347770e61175fbb
Geovanna74 <geovanacampos1403@gmail.com>
2025-05-28 21:56:31 -0300

    feat:Harry Potter

--- ola/src/App.jsx
+++ ola/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Body from './components/Body'
 import Header from './components/Header'
 
 function App() {
-
+  const [characters, setCharacters] = useState([])
+useEffect(() =>{
+ fetch('https://hp-api.onrender.com/api/characters')
+ .then((res) => res.json())
+ .then((res)=> setCharacters(res))
+ .catch((err) => console.error('Erro na API', err))
+},[])
  
   return (
     <div className='App'>
      <Header />
-     <Body  />
+     <Body  characters={characters}/>
 
     </div>
   )
--- ola/src/components/Body.jsx
+++ ola/src/components/Body.jsx
@@ -1,65 +1,29 @@
 import { useState } from 'react'
 import './Body.css'
-function Body() {
-  const[cont,setCont] = useState(0)
-  const[inputValor,setIpuntValor] = useState('')
-  const[inputN1,setIpuntN1] = useState('')
-  const[inputN2,setIpuntN2] = useState('')
-
-  function aumentar(){
-     setCont(cont + 1)
-  }
-  function diminuir(){
-    if(cont > 0){
-      setCont(cont - 1)
-    }
-  }
-  function alterar(){
-    setCont(Number(inputValor))
-  }
-  function soma(){
-    const soma = Number(inputN1) + Number(inputN2)
-    setCont((soma))
-  }
-  function subT(){
-    const subT = Number(inputN1) - Number(inputN2)
-    setCont((subT))
-  }
-  function multiplicacao(){
-    const multiplicacao = Number(inputN1) * Number(inputN2)
-    setCont((multiplicacao))
-  }
+function Body(props) {
+  console.log(props.characters)
+ 
   return (
     <div className='Body'>
-        <h2 className='titulo2'>Dinos são legais</h2>
-        <div className='cont'>
-        <button onClick={diminuir} >-</button>
-        <p> {cont}</p>
-        <button onClick={aumentar}>+</button>
+      <div className='grid'>
+      { props.characters.map((characters) =>
+       <div className="card">
+        {characters.image ?(
+         <img src={characters.image} alt={characters.name} />
+        ): (
+          <div className="noImage">Imagem indisponível</div>
+        )
+        
+        }
+        
+         <h2>{characters.name}</h2>
+         <p>
+          <strong>Casa:</strong>
+          {characters.house || " Desconhecida"}
+         </p>
         </div>
-
-        <div className="form">
-          <input type="number" 
-           value={inputValor}
-           onChange={(e) => setIpuntValor(e.target.value)}
-          />
-          <button onClick={alterar}>Alterar</button>
+        )}
         </div>
-
-
-       <div className='Calculadora'>
-       <input type="text"
-       value={inputN1} 
-       onChange={(e) => setIpuntN1(e.target.value)}
-       />
-        <button onClick={soma}>+</button>
-        <button onClick={subT}>-</button>
-        <button onClick={multiplicacao}>*</button>
-        <input type="text"
-        value={inputN2} 
-        onChange={(e) => setIpuntN2(e.target.value)}
-        />
-       </div>
     </div>
   )
 }
